907df6ba9ee2782cd7425d849658a86c8eb8b590
George Simeonov <george@macbookpro.lan>
2026-08-26 02:59:59 -0500

    Test static segmented LEDs without BLE updates

--- config/src/gs_leds.c
+++ config/src/gs_leds.c
@@ -2,11 +2,6 @@
 #include <zephyr/device.h>
 #include <zephyr/drivers/led_strip.h>
 #include <zephyr/init.h>
-#include <zephyr/kernel.h>
-
-#include <zmk/ble.h>
-#include <zmk/event_manager.h>
-#include <zmk/events/ble_active_profile_changed.h>
 
 #define LED_COUNT 187
 
@@ -25,55 +20,28 @@ static void fill_range(int first, int last,
     }
 }
 
-static void set_fixed_zones(void)
-{
-    fill_range(16, 31,    51, 51, 51);
-    fill_range(32, 46,    51, 18,  0);
-    fill_range(47, 61,    32,  0, 51);
-    fill_range(62, 90,    51,  0,  0);
-    fill_range(91, 124,    0,  0, 51);
-    fill_range(125, 161,   0, 51,  0);
-    fill_range(162, 187,  51, 38,  0);
-}
-
-static void update_status_leds(void)
-{
-    if (zmk_ble_active_profile_is_connected()) {
-        fill_range(1, 15, 0, 32, 51);
-    } else {
-        fill_range(1, 15, 32, 0, 51);
-    }
-
-    (void)led_strip_update_rgb(strip, pixels, LED_COUNT);
-}
-
 static int gs_led_init(void)
 {
     if (!device_is_ready(strip)) {
         return -ENODEV;
     }
 
-    set_fixed_zones();
-    update_status_leds();
-    return 0;
+    /* 20% max brightness */
+
+    /* 1-15: Purple status zone, STATIC for this test */
+    fill_range(1, 15,     32,  0, 51);
+
+    /* Fixed color zones */
+    fill_range(16, 31,    51, 51, 51); /* White  */
+    fill_range(32, 46,    51, 18,  0); /* Orange */
+    fill_range(47, 61,    32,  0, 51); /* Purple */
+    fill_range(62, 90,    51,  0,  0); /* Red    */
+    fill_range(91, 124,    0,  0, 51); /* Blue   */
+    fill_range(125, 161,   0, 51,  0); /* Green  */
+    fill_range(162, 187,  51, 38,  0); /* Yellow */
+
+    /* ONE LED transfer at startup only */
+    return led_strip_update_rgb(strip, pixels, LED_COUNT);
 }
 
 SYS_INIT(gs_led_init, APPLICATION, 90);
-
-static void ble_led_update_work_handler(struct k_work *work)
-{
-    ARG_UNUSED(work);
-    update_status_leds();
-}
-
-K_WORK_DELAYABLE_DEFINE(ble_led_update_work, ble_led_update_work_handler);
-
-static int gs_ble_led_listener(const zmk_event_t *eh)
-{
-    ARG_UNUSED(eh);
-    k_work_reschedule(&ble_led_update_work, K_MSEC(500));
-    return 0;
-}
-
-ZMK_LISTENER(gs_ble_led_listener, gs_ble_led_listener);
-ZMK_SUBSCRIPTION(gs_ble_led_listener, zmk_ble_active_profile_changed);
